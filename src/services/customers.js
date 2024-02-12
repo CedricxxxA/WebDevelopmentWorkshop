f29d36d872647b49e89a64ada755f4ed4f57b030
David <david.michailis@plan-b-gmbh.com>
2024-02-12 15:29:10 +0100

    create Delete Function

--- src/services/customers.js
+++ src/services/customers.js
@@ -10,6 +10,20 @@ const customer = class {
 
 const customerList = [];
 
+customerList.push(new customer("KN-00-00-01", "David", "Michailis", "PlanB.", "Krumbacher"));
+customerList.push(new customer("KN-00-00-02", "Cedric", "Achatz", "PlanB.", "RedBull"));
+customerList.push(new customer("KN-00-00-03", "Hans", "Wurst", "PlanB.", "Fanta"));
+
 export function GetAllCustomers(){
     return customerList;
+}
+
+export function DeleteCustomer(kundennr) {
+    const indexToDelete = customerList.findIndex(customer => customer.KundenNummer === kundennr);
+    if (indexToDelete !== -1) {
+        customerList.splice(indexToDelete, 1);
+        console.log(`Kunde mit der Kundennummer ${kundennr} wurde gelöscht.`);
+    } else {
+        console.log(`Kunde mit der Kundennummer ${kundennr} wurde nicht gefunden.`);
+    }
 }
--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,5 @@
-import { GetAllCustomers } from "./customers.js";
+import { DeleteCustomer, GetAllCustomers } from "./customers.js";
 
+console.log(GetAllCustomers());
+console.log(DeleteCustomer("KN-00-00-02"));
 console.log(GetAllCustomers());
