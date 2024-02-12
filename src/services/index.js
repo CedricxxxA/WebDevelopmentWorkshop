59550114d0d64706ce8619870aca617e7591c30d
CedricxxxA <Cedric.achatz@gmail.com>
2024-02-12 15:46:40 +0100

    changed index.js

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
-import { DeleteCustomer, GetAllCustomers, GetCustomerById } from "./customers.js";
+import { DeleteCustomer, GetAllCustomers, GetCustomerById, CreateCustomer } from "./customers.js";
 
+CreateCustomer("KN-55-00-00","Spongebob","Schwammkopf","Krosse Krabbe", "Krabbenburger");
+CreateCustomer("KN-56-00-00","Patrik","Star","Arbeitsamt", "Arbeitslos");
+DeleteCustomer("KN-55-00-00");
 console.log(GetAllCustomers());
-console.log(DeleteCustomer("KN-00-00-02"));
-console.log(GetAllCustomers());
-console.log(GetCustomerById("KN-00-00-03"));
